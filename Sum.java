a94171a6c51c640b5f5b52214a1748484cd5ea82
TejasKoshti <tejaskoshti2000@gmail.com>
2024-12-22 08:50:19 +0530

    Sum Of first 100 even numbers

---
+++ Sum.java
@@ -0,0 +1,15 @@
+
+public class Sum {
+	
+	public static void main(String[] args) {
+		int sum=0;
+		
+	for(int i=2; i<=200; i=i+2) {
+			sum+=i;
+		}
+	
+		System.out.println("Sum of first 100 even numbers = " + sum);
+
+	}
+
+}
